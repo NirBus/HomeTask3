02852969fbbdcb06ba97d3ae70b5410196a37fc8
VenDiago <alizg@code.edu.az>
2022-03-01 01:34:31 +0400

    Add project files.

---
+++ Extension.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeTask3
+{
+    public static class Extension
+    {
+        #region Task1
+        public static string ToCapitalized(this string word)
+        {
+            string _word = "";
+            word = word.ToLower();
+            var newword = word.Split(' ');
+            for (int i = 0; i < newword.Length; i++)
+            {
+                newword[i] = newword[i][0].ToString().ToUpper() + newword[i].Substring(1);
+            }
+            _word = string.Join(" ", newword);
+
+            return _word;
+        }
+        #endregion
+        #region Task2 -
+        //public static SearchExtension(this string word)
+        //{
+        //    //var newword = word.Split(' ');
+        //    //for (newword.Length)
+
+        //}
+        #endregion
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HomeTask3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task1
+            string sentence = "exTensiON claSSi staTIc OLMalIdiR";
+            Console.WriteLine(sentence.ToCapitalized());
+            #endregion
+
+        }
+    }
+}
